48c5cdeea9c4019e094a976bcf5f7044b026d62a
Kaitlyn <kaykaycsc@gmail.com>
2026-04-18 18:24:41 -0400

    remove stuff

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.routes import auth, deployments, health, uploads, stuff
+from app.api.routes import auth, deployments, health, uploads
 
 api_router = APIRouter()
 api_router.include_router(health.router, tags=["health"])
