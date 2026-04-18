0e5d2e1f5a5dcf14ef86995822cbf3b56f21dcc2
Kaitlyn <kaykaycsc@gmail.com>
2026-04-18 18:23:30 -0400

    fix

--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -4,7 +4,6 @@ from app.api.routes import auth, deployments, health, uploads, stuff
 
 api_router = APIRouter()
 api_router.include_router(health.router, tags=["health"])
-api_router.include_router(stuff.router, prefix="/stuff", tags=["stuff"])
 api_router.include_router(auth.router)
 api_router.include_router(uploads.router)
 api_router.include_router(deployments.router)
